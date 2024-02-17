560acd187da36c097a9e01b180343f6c853a9244
Ofure004 <ofure.ehiremhen@stu.cu.edu.ng>
2024-02-17 15:14:40 +0100

    completed the organization dashboard

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -3,47 +3,41 @@ import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 
 const data = [
   {
-    name: 'Page A',
+    name: 'Jan-Feb',
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
+    name: 'Mar-Apr',
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
+    name: 'May-Jun',
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
+    name: 'Jul-Aug',
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
+    name: 'Sep-Oct',
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
+    name: 'Nov-Dec',
     uv: 2390,
     pv: 3800,
     amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+  }
 ];
 
 export default class Example extends PureComponent {
--- src/pages/dashboards/orgdashboard.jsx
+++ src/pages/dashboards/orgdashboard.jsx
@@ -5,6 +5,7 @@ import stats from '../../assets/icons/stats.svg'
 import expenses from '../../assets/icons/expenses.svg'
 import Exampleneg from '../../components/negativechart.jsx'
 import Table from '../../components/orgtable.jsx'
+import add from '../../assets/icons/add.svg'
 
 
 export const OrgDashboard = () => {
@@ -47,20 +48,42 @@ export const OrgDashboard = () => {
                         </div>
                     </div>
                 </div>
-                <div className='h-3/6 bg-yellow flex gap-x-4'>
+                <div className='h-3/6 flex gap-x-4'>
                     <div className='w-1/2 h-[400px] bg-white-400'>
                         <PureComponent/> 
                     </div>
-                    <div className='w-1/2 bg-red-400 flex flex-col p-8 gap-y-4'>
-                        <div className='p-4 h-1/2 '>First Div</div>
-                        <div className='p-4 h-1/2 bg-slate-100'>Second div</div>
+                    <div className='w-1/2 flex flex-col p-8 gap-y-4'>
+                        <div className='p-4 h-1/2 border-2 border-white-500 rounded-lg flex gap-2'>
+                            <div className='w-3/4 h-full '>
+                                <p className='text-2xl font-semibold mb-3 text-center font-display'>Employee Statistics</p>
+                                <div><span className='text-xl font-semibold font-display'>5 </span><span className='font-body'>new employees recruited this quarter 🎉🎉🙌</span></div>
+                                <div><span className='text-xl font-semibold font-display'>2 </span><span className='font-body'>employees lost this quarter 😢😢</span></div>
+                            </div>
+                            <div className='w-1/4 '>
+                                <Example2/>
+                            </div>
+                        </div>
+                        <div className='p-4 h-1/2 border-2 border-white-500 rounded-lg '>
+                            <p className='text-2xl font-semibold mb-3 text-center font-display'>Upcoming Announcements!!! 🎊</p>
+                            <div className=''>
+                                <p className='font-body'>FC Trade is 20 tomorrow!!!!!!</p>
+                                <p className='font-body'>Important investment meeting happening on Monday</p>
+                            </div>
+                        </div>
+                      
                     </div>
                 </div>
                 <div className='h-2/6'>
-                        <div className='flex justify-between'>
-                            <p>Employee Table</p>
-                            <div className='px-[2px] py-2  bg-green-300 align-middle rounded-xl'>
-                                <p className='text-green-400 font-semibold'> terminate </p>
+                        <div className='flex justify-between items-center'>
+                            <p className="text-2xl font-body font-bold">Employee Table</p>
+                            <div className="flex gap-2 mb-2">
+                                <div className='px-8 py-2  bg-green-300 align-middle rounded-xl'>
+                                    <p className='text-green-400 font-body font-semibold'> Pay all</p>
+                                </div>
+                                <div className='flex gap-1 border-2 border-white-500 rounded-lg p-2'>
+                                    <img src={add}/>
+                                    <p className='font-body'>Add employee</p>
+                                </div>
                             </div>
                             
                         </div>
